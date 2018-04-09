9cdd59a232edfc3f127919bbbd0cb920ef28613b
Enis Gunesen <egunesen@microsoft.com>
2018-04-09 18:00:10 +0300

    Add project files.

---
+++ SlowRabbit.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlowRabbit.Mvc.MvcApplication" Language="C#" %>
---
+++ SlowRabbit.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlowRabbit.Mvc.Startup))]
+namespace SlowRabbit.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
